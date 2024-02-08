65409358e78da209f547a26c5ca66819404b6e60
Walnut <39544927+Walnut356@users.noreply.github.com>
2024-02-07 23:56:47 -0600

    chapter 4

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,92 @@
 #![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 #![feature(prelude_2024)]
+#![reexport_test_harness_main = "test_main"]
 
+pub mod serial;
 pub mod vga_buffer;
 
-
-
 pub mod prelude {
+    pub use crate::{
+        print, println,
+        serial::SERIAL1,
+        serial_print, serial_println,
+        vga_buffer::{BUFFER_HEIGHT, BUFFER_WIDTH, VGA_WRITER},
+        QemuExitCode,
+        exit_qemu,
+        Testable,
+
+    };
     pub use core::fmt::Write;
-    pub use crate::vga_buffer::{VGA_WRITER, BUFFER_HEIGHT, BUFFER_WIDTH};
-    pub use crate::print;
-    pub use crate::println;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+use core::panic::PanicInfo;
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+/// Entry point for `cargo test`
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+#![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
 use rust_os::prelude::*;
 
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    test_main();
+    write!(VGA_WRITER.lock(), "Eef freef\nSbubby\n{}", 8.0 / 3.15).unwrap();
+
+    loop {}
+}
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    write!(VGA_WRITER.lock(), "Eef freef\nSbubby\n{}", 8.0 / 3.15).unwrap();
-
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(QemuExitCode::Success);
 }
